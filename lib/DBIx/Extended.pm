fb0642b21e516f7b14591c8ab68ab4f26c19e106
punytan <punytan@gmail.com>
2011-10-26 01:27:30 +0900

    initial commit

---
+++ lib/DBIx/Extended.pm
@@ -0,0 +1,32 @@
+package DBIx::Extended;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Extended -
+
+=head1 SYNOPSIS
+
+  use DBIx::Extended;
+
+=head1 DESCRIPTION
+
+DBIx::Extended is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
